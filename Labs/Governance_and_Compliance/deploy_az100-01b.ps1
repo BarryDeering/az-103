fb6def7a8e761dfc42eaed69f4dbba98ad5565db
barry.deering@gmail.com <deeringb@cubic.com>
2020-01-28 13:39:52 +0000

    Commit-LABS

---
+++ Labs/Governance_and_Compliance/deploy_az100-01b.ps1
@@ -0,0 +1,4 @@
+New-AzResourceGroupDeployment `
+-ResourceGroupName "rg-bwdlab-az103" `
+-TemplateFile "C:\Az103\Labs\Governance_and_Compliance\az-100-01b_azuredeploy.code-workspace.json" `
+-TemplateParameterFile "C:\Az103\Labs\Governance_and_Compliance\az-100-01b_azuredeploy.parameters.json"
