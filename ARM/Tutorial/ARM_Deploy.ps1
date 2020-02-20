f9d42721192f9416e3487500630ccde290b0c244
barry.deering@gmail.com <deeringb@cubic.com>
2020-02-20 16:09:40 +0000

    ARM Tutorial 20-02-20

--- ARM/Tutorial/ARM_Deploy.ps1
+++ ARM/Tutorial/ARM_Deploy.ps1
@@ -1,8 +1,23 @@
-$templateFile = "C:\BWDGit\az-103\ARM\Tutorial\azuredeploy.json"
+<#$templateFile = "C:\BWDGit\az-103\ARM\Tutorial\azuredeploy.json"
 New-AzResourceGroupDeployment `
-  -Name add_name_variable `
-  -ResourceGroupName rg-bwdlab-az103 `
+  -Name app_service_plan `
+  -ResourceGroupName rg-bwdlab-AppServicePlan `
   -TemplateFile $templateFile `
-  -StorageSKU "Standard_LRS"
+  -StorageSKU "Standard_LRS" `
+  -ASP_Name "asp-bwdlab-ServicePlan" `
+  -StoragePrefix "bwdlab"
 
   #Comment from work PC
+
+  #>
+
+  $templateFile = "C:\BWDGit\az-103\ARM\Tutorial\azuredeploy.json"
+  $parameterFile="C:\BWDGit\az-103\ARM\Tutorial\azuredeploy.parameters.dev.json"
+New-AzResourceGroup `
+  -Name myResourceGroupDev `
+  -Location "UK South"
+New-AzResourceGroupDeployment `
+  -Name devenvironment `
+  -ResourceGroupName myResourceGroupDev `
+  -TemplateFile $templateFile `
+  -TemplateParameterFile $parameterFile
