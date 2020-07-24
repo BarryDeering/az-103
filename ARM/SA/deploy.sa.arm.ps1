2cb99f2307e28e3f77dc88240fb5a17785768cf2
Barry Deering <barry.deering@gmail.com>
2020-07-24 13:11:05 +0100

    latest HLT

---
+++ ARM/SA/deploy.sa.arm.ps1
@@ -0,0 +1,4 @@
+New-AzResourceGroupDeployment `
+-ResourceGroupName "rg-bwdlab-arm" `
+-TemplateFile "C:\BWDGit\az-103\ARM\SA\arm.sa.json" `
+-TemplateParameterFile "C:\BWDGit\az-103\ARM\SA\arm.sa.parameters.json"
